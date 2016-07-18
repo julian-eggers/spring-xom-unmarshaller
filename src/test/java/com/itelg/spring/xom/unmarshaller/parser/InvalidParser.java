f1e5304017077c3cec13eadad0174fc8ff2aca55
Julian Eggers <eggers.julian@gmail.com>
2016-07-18 20:51:42 +0200

    missing test added

---
+++ src/test/java/com/itelg/spring/xom/unmarshaller/parser/InvalidParser.java
@@ -0,0 +1,13 @@
+package com.itelg.spring.xom.unmarshaller.parser;
+
+import nu.xom.Element;
+
+public class InvalidParser implements Parser<Object>
+{
+    @Override
+    public Object parse(Element rootElement)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
--- src/test/java/com/itelg/spring/xom/unmarshaller/parser/ParserAnalyzerTest.java
+++ src/test/java/com/itelg/spring/xom/unmarshaller/parser/ParserAnalyzerTest.java
@@ -3,10 +3,6 @@ package com.itelg.spring.xom.unmarshaller.parser;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.itelg.spring.xom.unmarshaller.parser.Parser;
-import com.itelg.spring.xom.unmarshaller.parser.ParserAnalyzer;
-import com.itelg.spring.xom.unmarshaller.parser.ParserHolder;
-
 public class ParserAnalyzerTest
 {
     @Test
@@ -14,7 +10,7 @@ public class ParserAnalyzerTest
     {
         Parser<?> parser = new RootTagMatcherParser();
         ParserHolder holder = ParserAnalyzer.analyze(parser);
-        
+
         Assert.assertEquals(parser, holder.getParser());
         Assert.assertEquals(String.class, holder.getReturnType());
         Assert.assertEquals(3, holder.getSupportedRootTags().size());
@@ -22,16 +18,23 @@ public class ParserAnalyzerTest
         Assert.assertTrue(holder.getSupportedRootTags().contains("Double"));
         Assert.assertTrue(holder.getSupportedRootTags().contains("Integer"));
     }
-    
+
     @Test
     public void testAnalyzeRootTagTypeMatchingDisabledParser()
     {
         Parser<?> parser = new RootTagTypeMatchingDisableParser();
         ParserHolder holder = ParserAnalyzer.analyze(parser);
-        
+
         Assert.assertEquals(parser, holder.getParser());
         Assert.assertEquals(Integer.class, holder.getReturnType());
         Assert.assertEquals(1, holder.getSupportedRootTags().size());
         Assert.assertTrue(holder.getSupportedRootTags().contains("Number"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testAnalazyInvalidParser()
+    {
+        Parser<?> parser = new InvalidParser();
+        ParserAnalyzer.analyze(parser);
     }
 }
